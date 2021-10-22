a927d697adba8fc0defcb6cebaf93f16d3de2075
Santiago Téllez <santiago99tellez@gmail.com>
2021-10-22 16:00:29 -0500

    Table sales record fixed

--- Sprint_3/front_product_register/src/components/SalesRecord/SalesRecord.js
+++ Sprint_3/front_product_register/src/components/SalesRecord/SalesRecord.js
@@ -1,8 +1,14 @@
 import './SalesRecord.css';
+import React, { useState, useEffect } from 'react'
 import Ownfooter from '../Footer/Ownfooter'
 import Ownheader from '../Header/Ownheader'
 import { NavLink } from 'react-router-dom';
 import Actualdate from '../ActualDate/Actualdate.js';
+import { Table, TableHead, TableCell, TableRow, TableBody, Button} from '@material-ui/core';
+import { getProducts, deleteProduct } from '../../services/ProductService';
+import { makeStyles } from '@material-ui/styles';
+import { Link } from 'react-router-dom';
+import Container from '../Container/Container';
 
 
 function getCurrentDate(separator='/'){
@@ -14,16 +20,51 @@ function getCurrentDate(separator='/'){
   
   return `${date}${separator}${month<10?`0${month}`:`${month}`}${separator}${year}`
 }
-
-
-var data = [
-    {saleid: 1, productname: 'Gob', productid: '2', unitaryprice: '$50.000', date: getCurrentDate(), sale: '2', subtotal: 'npi'},
-    {saleid: 1, productname: 'Gob', productid: '2', unitaryprice: '$50.000', date: getCurrentDate(), sale: '2', subtotal: 'npi'},
-    {saleid: 1, productname: 'Gob', productid: '2', unitaryprice: '$50.000', date: getCurrentDate(), sale: '2', subtotal: 'npi'},
-];
-  
+const useStyles = makeStyles({
+  table: {
+      width: '95%',
+      margin: '1px 0 0 30px'
+  },
+  thead: {
+      '& > *': {
+          width: 'auto',
+          fontSize: 17,
+          background: '#2bb4c8',
+          color: '#FFFFFF',
+          family: 'Nunito,Helvetica,Arial,sans-serif'
+      }
+  },
+  row: {
+      '& > *': {
+          fontSize: 15,
+          color: '#1a497a',
+          family: 'Nunito,Helvetica,Arial,sans-serif'
+      }
+  }
+})
 
 function SalesRecord() {
+  const classes = useStyles();
+
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+      getAllProducts();
+  }, [])
+
+  const getAllProducts = async () => {
+      let response = await getProducts();
+      console.log(response);
+      setProducts(response.data.data);
+  }
+
+  const deleteProductData = async (id) => {
+      let callbackUser = window.confirm('Esta seguro de elimar el prudcto');
+      if (callbackUser) {
+          await deleteProduct(id);
+          getAllProducts();
+      }
+  }
   return (
     <div className="App">
       <Ownheader/>
@@ -47,7 +88,34 @@ function SalesRecord() {
             </form>
         </div>
         <div className = "BotomContainer">
-            <h1>Titulo</h1>
+            <Table className={classes.table}>
+                            <TableHead>
+                                <TableRow className={classes.thead}>
+                                    <TableCell>Id Product</TableCell>
+                                    <TableCell>Description</TableCell>
+                                    <TableCell>Value</TableCell>
+                                    <TableCell>State</TableCell>
+                                    <TableCell></TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {
+                                    products.map(product => (
+                                        <TableRow className={classes.row} key={product._id}>
+                                            <TableCell>{product._id}</TableCell>
+                                            <TableCell>{product.description}</TableCell>
+                                            <TableCell>{product.value}</TableCell>
+                                            <TableCell>{product.state ? "In-Stock" : "Out-of-stock"}</TableCell>
+                                            <TableCell>
+                                                <Button component={Link} to={`/edit/${product._id}`} color="primary">Edit</Button>
+                                                <Button color="secondary" onClick={() => deleteProductData(product._id)} >Remove</Button>
+                                            </TableCell>
+                                        </TableRow>
+                                    ))
+                                }
+                            </TableBody>
+                            
+                        </Table>
         </div>
       </div>
       <Ownfooter/>
