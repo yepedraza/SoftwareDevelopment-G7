b9c3e4c99786d954a823b1c87e37f1cebb1577a8
Santiago Téllez <santiago99tellez@gmail.com>
2021-10-23 00:07:01 -0500

    Daniel Fixed

--- Sprint_3/front_product_register/src/services/UsersService.js
+++ Sprint_3/front_product_register/src/services/UsersService.js
@@ -6,3 +6,6 @@ const usersUrl = "http://localhost:3002/usuarios";
 export const createUser = async (user) => {
     return await axios.post(`${usersUrl}/`, user);
 }
+export const editUser = async (user) => {
+    return await axios.put(`${usersUrl}/${user._id}`, user);
+}
